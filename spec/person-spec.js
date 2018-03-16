07c121fd9ee30f60397c86f9e262b656a0a366e5
tyler kostelak <tkostelak@gmail.com>
2018-03-16 15:31:06 -0700

    dateToSeconds function successfully passes tests.

---
+++ spec/person-spec.js
@@ -0,0 +1,15 @@
+import { dateToSeconds } from '../src/person.js';
+
+const secondsInMonth = 2628000;
+const secondsInYear = 31540000;
+const secondsInDay = 86400;
+
+describe('dateToSeconds', function() {
+  it('should test if a date is successfully converted into seconds', function() {
+    let year = 2018;
+    let month = 3;
+    let day = 16;
+    let dateInSeconds = (year * secondsInYear) + (month * secondsInMonth) + (day * secondsInDay);
+    expect(dateInSeconds).toEqual(63656986400);
+  });
+});
