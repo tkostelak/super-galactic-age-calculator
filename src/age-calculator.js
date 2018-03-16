2b69f6ade40962c8bc1b2d94904a165651fc118b
tyler kostelak <tkostelak@gmail.com>
2018-03-16 13:31:51 -0700

    proper karma configuration for windows 10 adds ability to run tests, and README adds outline for specs to begin building the backend logic.

---
+++ src/age-calculator.js
@@ -0,0 +1,25 @@
+1. First, a user should be able to input their age and it should be converted into seconds.
+
+2. Next, it should be possible to convert any given date into seconds.
+
+3. Once a date can be converted into seconds, the app should be able to find the difference between two dates.
+
+4. After the difference can be found between two dates, the user should be able to input their birthday and the current date and the app should return their exact age.
+
+5. Now that the app can calculate an exact age, the user should be able to convert their age into mercury years.
+
+6. Then they should be able to convert their age into Venus years.
+
+7. Then they should be able to convert their age into Mars years.
+
+8. Then they should be able to convert their age into Jupiter years.
+
+9. Once the users age can be converted into Mercury years, then they should be able to input their demographics average life expectancy and determine their Mercury life expectancy.
+
+10. Once the users age can be converted into Venus years, then they should be able to input their demographics average life expectancy and determine their Venus life expectancy.
+
+11. Once the users age can be converted into Mars years, then they should be able to input their demographics average life expectancy and determine their Mars life expectancy.
+
+12. Once the users age can be converted into Jupiter years, then they should be able to input their demographics average life expectancy and determine their Jupiter life expectancy.
+
+13. Once a user is able to have their respective galactiv life expectancies caclulated, if their life expectancy is less than their age, then they should receive a congralutory message.
