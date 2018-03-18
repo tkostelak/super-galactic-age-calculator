745c30418f5ed770f83e39c3507b753200dc803e
tyler kostelak <tkostelak@gmail.com>
2018-03-18 16:20:54 -0700

    calculate life expectancy function passes all tests and adds ability to calculate and return users life expectancy for a variety of planets and based on gender.

--- spec/human-spec.js
+++ spec/human-spec.js
@@ -1,23 +1,28 @@
 import { Human } from '../src/human.js';
 
+const milliToYear = 31556736000;
 const mercuryYear = 88;
 const venusYear = 225;
 const marsYear = 687;
+const earthYear = 1;
 const jupiterYear = 4380;
+const maleLifeExpect = 76;
+const femaleLifeExpect = 81;
 
 //This test passing confirms that the constructor is returning the values that I am expecting it to.
 describe('Human', function() {
   it('should test whether or not a Human has its properties', function() {
-    let testHuman = new Human("Rick Sanchez", ("October 13, 2014"));
+    let testHuman = new Human("Rick Sanchez", ("October 13, 2014"), "male");
     expect(testHuman.name).toEqual("Rick Sanchez");
     expect(testHuman.birthDate).toEqual("October 13, 2014");
+    expect(testHuman.gender).toEqual("male");
   });
 });
 
 //In order to calculate a users age, first a new human needs to be instantiated. The function is always going to return a year because of the math floor in the function.
 describe('ageCalculator', function() {
   it('should test if the users exact age is returned', function() {
-    let testHuman = new Human("Rick Sanchez", ("August 31, 1991"));
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1991"), "male");
     let newAge = testHuman.ageCalculator();
     expect(newAge).toEqual(26);
   });
@@ -26,7 +31,7 @@ describe('ageCalculator', function() {
 //Initially each planetary age was going to be a different SPEC, but it felt overly repetitive and was DRYED to a function that takes planetaryYear as a parameter.
 describe('planetAgeCalculator', function() {
   it('should test if the users age is returned in Mercury years', function() {
-    let testHuman = new Human("Rick Sanchez", ("August 31, 1991"));
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1991"), "male");
     let newPlanetaryAge = testHuman.planetAgeCalculator(mercuryYear);
     expect(newPlanetaryAge).toEqual(107);
     expect(newPlanetaryAge).not.toEqual(26);
@@ -36,7 +41,7 @@ describe('planetAgeCalculator', function() {
 //Testing same function planetAgeCalculator but for Venus Years
 describe('planetAgeCalculator', function() {
   it('should test if the users age is returned in Venus years', function() {
-    let testHuman = new Human("Rick Sanchez", ("August 31, 1991"));
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1991"), "male");
     let newPlanetaryAge = testHuman.planetAgeCalculator(venusYear);
     expect(newPlanetaryAge).toEqual(42);
     expect(newPlanetaryAge).not.toEqual(26);
@@ -46,7 +51,7 @@ describe('planetAgeCalculator', function() {
 //Testing same function planetAgeCalculator but for Mars Years
 describe('planetAgeCalculator', function() {
   it('should test if the users age is returned in Venus years', function() {
-    let testHuman = new Human("Rick Sanchez", ("August 31, 1991"));
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1991"), "male");
     let newPlanetaryAge = testHuman.planetAgeCalculator(marsYear);
     expect(newPlanetaryAge).toEqual(13);
     expect(newPlanetaryAge).not.toEqual(26);
@@ -56,9 +61,47 @@ describe('planetAgeCalculator', function() {
 //Testing same function planetAgeCalculator but for Jupiter Years
 describe('planetAgeCalculator', function() {
   it('should test if the users age is returned in Venus years', function() {
-    let testHuman = new Human("Rick Sanchez", ("August 31, 1991"));
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1991"), "male");
     let newPlanetaryAge = testHuman.planetAgeCalculator(jupiterYear);
     expect(newPlanetaryAge).toEqual(2);
     expect(newPlanetaryAge).not.toEqual(26);
+  });
+});
+
+//This test is to determine if the function is returning the correct life expectancy in years. The function takes in planetary years as a parameter, and the user will have a different result depending on which gender they choose. This test is for male.
+describe('planetLifeExpectancy', function() {
+  it('should test if the users planetary life expectancy is returned', function() {
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1991"), "male");
+    let lifeExpectancy = testHuman.planetLifeExpectancy(earthYear);
+    expect(lifeExpectancy).toEqual(50);
+    expect(lifeExpectancy).not.toEqual(55);
+  });
+});
+
+//Testing the same function as the previous test but the gender is changed to female.
+describe('planetLifeExpectancy', function() {
+  it('should test if the users planetary life expectancy is returned', function() {
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1991"), "female");
+    let lifeExpectancy = testHuman.planetLifeExpectancy(earthYear);
+    expect(lifeExpectancy).toEqual(55);
+    expect(lifeExpectancy).not.toEqual(50);
+  });
+});
+
+//Testing the same function but where the users life expectancy is equal to their age so a semi-ominoue message should be returned.
+describe('planetLifeExpectancy', function() {
+  it('should test if an ominous message is returned', function() {
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1941"), "male");
+    let lifeExpectancy = testHuman.planetLifeExpectancy(earthYear);
+    expect(lifeExpectancy).toEqual("Yikes! The end may be near for you, or you have many long rewarding years to follow.");
+  });
+});
+
+//Testing the same function but where the users life expectancy is less than their age so a congratulatory message is returned.
+describe('planetLifeExpectancy', function() {
+  it('should test if an ominous message is returned', function() {
+    let testHuman = new Human("Rick Sanchez", ("August 30, 1935"), "male");
+    let lifeExpectancy = testHuman.planetLifeExpectancy(earthYear);
+    expect(lifeExpectancy).toEqual("Congrats! You have already surpassed the life expectancy by -6 years!");
   });
 });
--- src/human.js
+++ src/human.js
@@ -2,13 +2,16 @@ const milliToYear = 31556736000;
 const mercuryYear = 88;
 const venusYear = 225;
 const marsYear = 687;
+const earthYear = 1;
 const jupiterYear = 4380;
-
+const maleLifeExpect = 76;
+const femaleLifeExpect = 81;
 
 export class Human {
-  constructor(name, birthDate) {
+  constructor(name, birthDate, gender) {
     this.name = name;
     this.birthDate = birthDate;
+    this.gender = gender;
   }
 
 //Built in javascript function DATE is able to either convert a specified date string into a date or can pull the exact current date. Output defaults to milliseconds, so the age is converted to years and the nearest whole number in order to be user friendly.
@@ -21,10 +24,24 @@ export class Human {
 
 //This function calculates the users age just like age calculator, but then also converts the age to whichever planet the user would like and outputs whole number age for user friendliness.
   planetAgeCalculator(planetYear) {
-    let birthDate = new Date(this.birthDate);
-    let dateDifference = Date.now() - birthDate;
-    let age = Math.floor(dateDifference / milliToYear);
+
+    let age = this.ageCalculator();
     let planetaryAge = Math.floor((age*365) / planetYear);
     return planetaryAge;
+  }
+
+  //This function will determine a users life expectancy in either mercury, venus, mars, or jupiter years. To keep it simple, the app will only ask the user for their gender, and will use average life expectancies for their gender in the United States. If the users lift expectancy is less than, they receive a congrtulatory message, they receive an ominous message for equal to 0, and they just receive the years left for everything else.
+
+  planetLifeExpectancy(planetYear) {
+
+  let age = this.ageCalculator();
+  let yearsLeft = 0;
+
+  if(this.gender === "female") yearsLeft += ((femaleLifeExpect / planetYear) - age);
+  if(this.gender === "male") yearsLeft += ((maleLifeExpect / planetYear) - age);
+
+  if(yearsLeft === 0) return "Yikes! The end may be near for you, or you have many long rewarding years to follow."
+  if(yearsLeft <= 0) return "Congrats! You have already surpassed the life expectancy by " + yearsLeft + " years!"
+  else return yearsLeft;
   }
 }
